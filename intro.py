91f6f094f3af18d36fb80bab888f09fa3fca08da
Evil Enkidu <maganae@uabc.edu.mx>
2024-08-31 08:06:40 -0700

    First commit

---
+++ intro.py
@@ -0,0 +1,19 @@
+print("Hello from PYTHON")
+array = [1,2,3,4,5,6,6,7]
+print(array)
+array.append(array)
+print(array)
+arrayarray = [array]
+arrayarray.append(arrayarray)
+print(arrayarray)
+
+def hello():
+    print("'Ello from pythen")
+
+hello()
+
+dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1969
+}
