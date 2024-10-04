08556133349bd8aaf20066a296214e0692740559
Evil Enkidu <maganae@uabc.edu.mx>
2024-10-04 11:08:58 -0700

    FSDI 110 Commit

--- server/server.py
+++ server/server.py
@@ -1,9 +1,11 @@
-
-from config import dataBase
+from flask import Flask, request
+import json
+from config import dataBase  
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-product_list = []  
 def fix_id(obj):
     obj["_id"] = str(obj["_id"])
     return obj
@@ -24,9 +26,9 @@ def footer():
 
 @app.get("/api/products")
 def get_products():
-    # Retrieve products from database
-    products = list(dataBase.product_list.find())
-    return json.dumps([fix_id(product) for product in products])
+    # Retrieve products from the database
+    productsCursor = list(dataBase.catalog.find({}))
+    return json.dumps([fix_id(product) for product in productsCursor])
 
 @app.get("/api/product/count")
 def get_product_count():
@@ -37,18 +39,31 @@ def get_product_count():
 @app.post("/api/products")
 def save_products():
     item = request.get_json()
-    dataBase.product_list.insert_one(item)
+    dataBase.catalog.insert_one(item)
     print(item)
     return json.dumps(fix_id(item))
 
 @app.put("/api/products/<int:index>")
 def update_products(index):
     update_item = request.get_json()
-    if 0 <= index < len(product_list):
-        product_list[index] = update_item
-        return json.dumps(update_item)
-    else:
-        return "That index doesn't exist", 404
+    # You don't need to update the local product_list unless you're using it elsewhere
+    return json.dumps(update_item)
+
+
+# Post new Coupon 
+@app.post("/api/coupons")
+def save_coupon():
+    new_coupon = request.get_json()
+    dataBase.coupons.insert_one(new_coupon)
+    print(new_coupon)
+    return json.dumps(fix_id(new_coupon))
+
+
+# Retrieve coupons from the database
+@app.get("/api/coupons")
+def get_coupons():
+    couponsCursor = list(dataBase.coupons.find({}))
+    return json.dumps([fix_id(coupon) for coupon in couponsCursor])
 
 if __name__ == "__main__":
     app.run(debug=True)
